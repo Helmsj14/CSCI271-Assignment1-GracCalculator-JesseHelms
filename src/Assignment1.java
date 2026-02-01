628b1a75012d5d671b45edbce189966f44747089
Helmsj14 <dillonhelms06@gmail.com>
2026-01-31 21:33:17 -0500

    start assignment

---
+++ src/Assignment1.java
@@ -0,0 +1,9 @@
+public class Assignment1 {
+    public static void main(String[] args) throws Exception {
+    int Finalgrade=0;
+    int Midtermgrade=0;
+    int Assignmentgrade=0;
+    int testgrade=0;
+    
+    }
+}
